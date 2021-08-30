202eab6ddfa7d1c4569c0d75e32aed2f15286140
Jap <jeet63240@gmail.com>
2021-08-30 19:51:55 +0530

    Adding Some Statement & make output visible

--- class_in_one_line.py
+++ class_in_one_line.py
@@ -3,9 +3,12 @@ This is a python class in one line using type meta class.
 Example usage:
 
 robo = Robot()
-robo.say_hello("Anyesh")
+robo.say_hello("Jeet")
 
-Output: Hi, I am Anyesh
+Output: Hi, I am Jeet
 '''
 
 Robot = type("Robot", (), { "say_hello": lambda self, name: "Hi, I am " + name, },)
+robo = Robot()
+
+print(robo.say_hello("Jeet"))
