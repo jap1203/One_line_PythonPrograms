2ea3eeba11dedeed5147fc4866a4792244d7bee4
Jap <jeet63240@gmail.com>
2021-08-30 20:29:03 +0530

    Edit Files As it is

--- averageGivenList.py
+++ averageGivenList.py
@@ -4,8 +4,4 @@ Example:
 input-3 6 2 9 5
 Output-5.0
 '''
-list1 = [2,6,4,5,8,1,9]
-
-avgList=sum(list1)/len(list1)
-
-print(avgList)
+avgList=sum(list)/len(list)
--- class_in_one_line.py
+++ class_in_one_line.py
@@ -3,12 +3,9 @@ This is a python class in one line using type meta class.
 Example usage:
 
 robo = Robot()
-robo.say_hello("Jeet")
+robo.say_hello("Ayush")
 
-Output: Hi, I am Jeet
+Output: Hi, I am Ayush
 '''
 
 Robot = type("Robot", (), { "say_hello": lambda self, name: "Hi, I am " + name, },)
-robo = Robot()
-
-print(robo.say_hello("Jeet"))
